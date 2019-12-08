d4021f91f00b5290c73e9cddd8a2a65d140eb9de
EdwardKerckhofHogent <edward.kerckhof@student.hogent.be>
2019-12-08 14:50:46 +0100

    [IOS]: Programatic UI setup

--- fitness-app-ios/fitness-app-ios/LoginViewController.swift
+++ fitness-app-ios/fitness-app-ios/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  fitness-app-ios
 //
 //  Created by Edward Kerckhof on 08/12/2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
     }
 
-
 }
-
