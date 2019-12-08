78c680e0db89f7027b427701ec03913eb60b5694
EdwardKerckhofHogent <edward.kerckhof@student.hogent.be>
2019-12-08 21:34:20 +0100

    [IOS]: GraphQL integration and simple form falidation

---
+++ fitness-app-ios/fitness-app-ios/Utils/Validator.swift
@@ -0,0 +1,48 @@
+//
+//  Validator.swift
+//  fitness-app-ios
+//
+//  Created by Edward Kerckhof on 08/12/2019.
+//  Copyright © 2019 Edward Kerckhof. All rights reserved.
+//
+
+// URL: https://dev.to/onmyway133/how-to-make-simple-form-validator-in-swift-1hlg met eigen aanpassingen
+
+import Foundation
+import UIKit
+
+class Validator {
+    func validate(text: String, with rules: [Rule]) -> String? {
+        return rules.compactMap({ $0.check(text) }).first
+    }
+
+    func validate(input: UITextField, with rules: [Rule]) -> Bool {
+        guard let message = validate(text: input.text ?? "", with: rules) else { return true }
+        if !input.isSecureTextEntry {
+            input.text = message
+        }
+        return false
+    }
+}
+
+struct Rule {
+    // Return nil if matches, error message otherwise
+    let check: (String) -> String?
+
+    static let notEmpty = Rule(check: {
+        return $0.isEmpty ? "Verplicht" : nil
+    })
+    
+    static func minLength(length: Int) -> Rule {
+        Rule(check: {
+            return $0.count < length ? "Min. lengte van: \(length)" : nil
+        })
+    }
+
+    static let validEmail = Rule(check: {
+        let regex = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
+
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: $0) ? nil : "Geen geldig email"
+    })
+}
--- fitness-app-ios/fitness-app-ios/ViewControllers/AuthViewController.swift
+++ fitness-app-ios/fitness-app-ios/ViewControllers/AuthViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class AuthViewController: UIViewController {
     @IBOutlet var loginStackView: UIStackView!
-    @IBOutlet var registerStackView: UIStackView!
+    @IBOutlet var loginEmailTexField: UITextField!
+    @IBOutlet var loginPasswordTextField: UITextField!
     @IBOutlet var goToRegistrationButton: UIButton!
+    
+    @IBOutlet var registerStackView: UIStackView!
+    @IBOutlet var registerEmailTextField: UITextField!
+    @IBOutlet var registerPasswordTextField: UITextField!
+    @IBOutlet var registerRepeatPasswordTextField: UITextField!
     @IBOutlet var goToLoginButton: UIButton!
+    
     @IBOutlet var errorLabels: [UILabel]!
     
+    let apollo = Network.shared.apollo
+    let validator = Validator()
     var hideRegister: Bool = true
+    var errorMessages: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,5 +43,40 @@ class AuthViewController: UIViewController {
         }
         loginStackView.isHidden = !hideRegister
         registerStackView.isHidden = hideRegister
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        errorMessages = []
+        
+        let loginEmailValidationResponse =  validator.validate(input: loginEmailTexField, with: [.notEmpty])
+        
+        let loginPasswordValidationResponse = validator.validate(input: loginPasswordTextField, with: [.notEmpty])
+        
+        if !loginEmailValidationResponse || !loginPasswordValidationResponse {
+            errorMessages.append("Form validatie fouten")
+            for error in errorMessages {
+                setErrorInLabel(label: 1, error: error)
+            }
+        } else {
+            print("Login")
+            apollo.perform(mutation: LoginMutation(input: UserInput.init(password: loginPasswordTextField.text!, email: loginEmailTexField.text!))) { result in
+                guard let data = try? result.get().data else { return }
+                if data.login == nil {
+                    self.setErrorInLabel(label: 1, error: "Verkeerd email/wachtwoord")
+                } else {
+                    // Successful login -> navigate to home
+                    self.performSegue(withIdentifier: "HomeSegue", sender: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        print("Register")
+    }
+    
+    func setErrorInLabel(label: Int, error: String) {
+        errorLabels[label].alpha = 1
+        errorLabels[label].text = error
     }
 }
