ff2ea657eee5ff004589903bd64118c425bfb180
EdwardKerckhofHogent <edward.kerckhof@student.hogent.be>
2019-12-08 14:38:52 +0100

    [IOS]: Project init

---
+++ fitness-app-ios/fitness-app-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness-app-ios
+//
+//  Created by Edward Kerckhof on 08/12/2019.
+//  Copyright © 2019 Edward Kerckhof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
